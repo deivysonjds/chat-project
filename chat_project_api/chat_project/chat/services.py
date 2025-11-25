29b0218c46db2ab831d03e302b207c626be5d2a0
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-11-25 06:25:11 -0300

    refactor: instruction to AI

--- chat_project_api/chat_project/chat/services.py
+++ chat_project_api/chat_project/chat/services.py
@@ -6,5 +6,5 @@ genai.configure(api_key=settings.GEMINI_API_KEY)
 model = genai.GenerativeModel("models/gemini-2.5-flash")
 
 def generate_response(prompt: str) -> str:
-    response = model.generate_content(f"Responda APENAS com texto puro, sem markdown, sem negrito, sem formatação, sem quebra de linha, etc:\n{prompt}")
+    response = model.generate_content(f"Responda APENAS com texto puro, sem markdown, sem negrito, sem símbolos:\n{prompt}")
     return response.text
