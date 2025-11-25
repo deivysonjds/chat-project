fc994529075c630ea18d8cfcc6d930ccec1359a1
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-11-25 03:26:48 -0300

    refactor

--- chat_project_frontend/middleware.ts
+++ chat_project_frontend/middleware.ts
@@ -1,4 +1,3 @@
-// app/proxy.ts
 import { NextRequest, NextResponse } from 'next/server'
 
 const protectedRoutes = ['/chat', '/history']
@@ -7,14 +6,13 @@ const publicRoutes = ['/']
 export default function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
   
-  const token = request.cookies.get('refreshToken')?.value
+  const token = request.cookies.get('tokenRefresh')?.value
   const isAuthenticated = !!token
-
-  console.log('Proxy - Path:', pathname, 'Authenticated:', isAuthenticated) // Debug
-
+  console.log(token);
+  
+  
   // Se tentar acessar rota protegida sem autenticação
   if (protectedRoutes.some(route => pathname.startsWith(route)) && !isAuthenticated) {
-    console.log('Redirecting to login from protected route')
     const loginUrl = new URL('/', request.url)
     loginUrl.searchParams.set('from', pathname)
     return NextResponse.redirect(loginUrl)
@@ -22,7 +20,6 @@ export default function middleware(request: NextRequest) {
 
   // Se autenticado e tentar acessar rota pública
   if (isAuthenticated && publicRoutes.some(route => pathname === route)) {
-    console.log('Redirecting to chat from public route')
     return NextResponse.redirect(new URL('/chat', request.url))
   }
 
