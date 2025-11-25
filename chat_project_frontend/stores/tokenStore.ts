89a917b03c51fbb54fcac7351700f9da1f3dc045
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-11-25 03:34:55 -0300

    added: global state management with persistence in cookies

--- chat_project_frontend/stores/tokenStore.ts
+++ chat_project_frontend/stores/tokenStore.ts
@@ -1,18 +1,55 @@
 import { create } from 'zustand'
-import { Message } from '../schemas/messageSchema'
+import Cookies from 'js-cookie'
+import { getUsernameByToken } from '@/services/decodeJwt'
 
 interface TokenState {
-  tokenRefresh: string | null,
-  tokenAccess: string | null,
-  login: (tokenAccess: string, tokenRefresh: string) => void,
-  logout: () => void,
-  refresh: (token: string) => void
+	tokenRefresh: string | null,
+	tokenAccess: string | null,
+	user: string | null,
+	login: (tokenAccess: string, tokenRefresh: string) => void,
+	logout: () => void,
+	refresh: (token: string) => void
 }
 
-export const useChatStore = create<TokenState>((set) => ({
-  tokenRefresh: null,
-  tokenAccess: null,
-  login: (tokenAccess: string, tokenRefresh) => set({tokenAccess: tokenAccess, tokenRefresh: tokenRefresh}),
-  logout: () => set({tokenRefresh: null}),
-  refresh: (tokenAccess: string) => set({tokenAccess: tokenAccess})
+export const cookieStorage = {
+	getItem: (name: string): string | null => {
+		if (typeof window === 'undefined') return null
+		return Cookies.get(name) || null
+	},
+	setItem: (name: string, value: string): void => {
+		if (typeof window === 'undefined') return
+		Cookies.set(name, value, {
+			expires: 7,
+			secure: true,
+			sameSite: 'strict',
+			path: '/',
+		})
+	},
+	removeItem: (name: string): void => {
+		if (typeof window === 'undefined') return
+		Cookies.remove(name, { path: '/' })
+	},
+}
+
+export const useTokenState = create<TokenState>((set) => ({
+	user: cookieStorage.getItem('user'),
+	tokenRefresh: cookieStorage.getItem('tokenRefresh'),
+	tokenAccess: cookieStorage.getItem('tokenAccess'),
+	login: (tokenAccess: string, tokenRefresh) => {
+		let username = getUsernameByToken(tokenRefresh)
+		cookieStorage.setItem('tokenAccess', tokenAccess)
+		cookieStorage.setItem('tokenRefresh', tokenRefresh)
+		cookieStorage.setItem('user', username)
+		set({ tokenAccess: tokenAccess, tokenRefresh: tokenRefresh, user: username})
+	},
+	logout: () => {
+		cookieStorage.removeItem('tokenAccess')
+		cookieStorage.removeItem('tokenRefresh')
+		cookieStorage.removeItem('user')
+		set({ tokenRefresh: null, tokenAccess: null,  user: null})
+	},
+	refresh: (tokenAccess: string) => {
+		cookieStorage.setItem('tokenAccess', tokenAccess)
+		set({ tokenAccess: tokenAccess })
+	}
 }))
